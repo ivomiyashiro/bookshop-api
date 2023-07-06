7dfff823c2a31f3a04d1d35949550de60b3011f5
Ivan Miyashiro <ivo.miyashiro1@gmail.com>
2023-07-06 10:33:37 -0300

    Add barrel file and tests file

---
+++ src/auth/index.ts
@@ -0,0 +1,7 @@
+export * from './auth.controller';
+export * from './auth.module';
+export * from './auth.service';
+export * from './decorator';
+export * from './dto';
+export * from './interface';
+export * from './strategy';
