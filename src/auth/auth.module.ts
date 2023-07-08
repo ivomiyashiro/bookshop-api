974a039b8788688ad515d5a4f318ca5b313fa409
Ivan Miyashiro <ivo.miyashiro1@gmail.com>
2023-07-08 12:22:18 -0300

    Add JwtStrategy

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
-import { AuthService, GoogleStrategy } from './';
+import { AuthService, GoogleStrategy, JwtStrategy } from './';
 import { AuthController } from './controller/auth.controller';
 
 @Module({
   imports: [JwtModule.register({ global: true }), PassportModule.register({})],
-  providers: [AuthService, GoogleStrategy],
+  providers: [AuthService, GoogleStrategy, JwtStrategy],
   controllers: [AuthController],
 })
 export class AuthModule {}
