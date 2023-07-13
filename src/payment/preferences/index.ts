8c2609a99031cdcaaa76a0e50d976585c1108ebd
Ivan Miyashiro <ivo.miyashiro1@gmail.com>
2023-07-13 20:09:43 -0300

    Add payment preferences

---
+++ src/payment/preferences/index.ts
@@ -0,0 +1,11 @@
+export const preferences = {
+  back_urls: {
+    success: 'https://www.success.com',
+    failure: 'http://www.failure.com',
+    pending: 'http://www.pending.com',
+  },
+  statement_descriptor: 'DEV BOOKSTORE',
+  payment_methods: {
+    installments: 3,
+  },
+};
