a864350e98d47a8a15b0846a860ebe1ba4e67e6c
Ivan Miyashiro <ivo.miyashiro1@gmail.com>
2023-07-13 20:08:31 -0300

    Add payment service implementation

---
+++ src/payment/pipe/items-data-validation.pipe.ts
@@ -0,0 +1,46 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import {
+  PipeTransform,
+  Injectable,
+  ArgumentMetadata,
+  BadRequestException,
+} from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class ItemsDataValidationPipe implements PipeTransform {
+  constructor(private prismaService: PrismaService) {}
+  async transform(value: any, _metadata: ArgumentMetadata) {
+    const { items } = value;
+
+    const productsIds = items.map((item) => Number(item.id));
+
+    const dbProducts = await this.prismaService.book.findMany({
+      where: { id: { in: productsIds } },
+    });
+
+    for (const item of items) {
+      const { id, unit_price, quantity } = item;
+
+      const dbItem = dbProducts.find((dbItem) => dbItem.id === Number(id));
+
+      if (!dbItem) {
+        throw new BadRequestException([
+          `Product with id ${id} does not exists`,
+        ]);
+      }
+
+      if (dbItem.price !== unit_price) {
+        throw new BadRequestException(
+          `The price given does not match with database`,
+        );
+      }
+
+      if (dbItem.stock < quantity) {
+        throw new BadRequestException(`Quantity requested is not available.`);
+      }
+    }
+
+    return value;
+  }
+}
