4fd29a444ecbd8c3d456c4c28a7c7298accae8f1
Ivan Miyashiro <ivo.miyashiro1@gmail.com>
2023-07-13 20:08:46 -0300

    Add payment dto

---
+++ src/payment/dto/ItemsToPay.dto.ts
@@ -0,0 +1,50 @@
+import {
+  IsString,
+  IsNumber,
+  IsUrl,
+  ValidateNested,
+  ArrayNotEmpty,
+  IsNotEmpty,
+  IsEnum,
+} from 'class-validator';
+
+enum Currency {
+  ARS = 'ARS',
+}
+
+class ItemDto {
+  @IsNotEmpty()
+  @IsString()
+  id: string;
+
+  @IsNotEmpty()
+  @IsString()
+  title: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @IsEnum(Currency)
+  currency_id: string;
+
+  @IsNotEmpty()
+  @IsUrl()
+  picture_url: string;
+
+  @IsNotEmpty()
+  @IsString()
+  description: string;
+
+  @IsNotEmpty()
+  @IsNumber()
+  quantity: number;
+
+  @IsNotEmpty()
+  @IsNumber()
+  unit_price: number;
+}
+
+export class ItemsToPayDto {
+  @ValidateNested({ each: true })
+  @ArrayNotEmpty()
+  items: ItemDto[];
+}
