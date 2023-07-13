146bcebb1a7a91698e59fae4e85d94c2cbc80bee
Ivan Miyashiro <ivo.miyashiro1@gmail.com>
2023-07-13 20:08:11 -0300

    Add payment controller implementation

---
+++ src/payment/controller/payment.controller.ts
@@ -0,0 +1,18 @@
+import { Controller, Post, Body } from '@nestjs/common';
+import { PaymentService } from '../services/payment.service';
+import { ItemsToPayDto } from '../dto';
+import { ItemsDataValidationPipe } from '../pipe';
+
+@Controller('/api/payment')
+export class PaymentController {
+  constructor(private paymentService: PaymentService) {}
+
+  @Post()
+  async createPayment(@Body(ItemsDataValidationPipe) dto: ItemsToPayDto) {
+    const payment_url = await this.paymentService.createPayment(dto);
+
+    return {
+      data: { payment_url },
+    };
+  }
+}
