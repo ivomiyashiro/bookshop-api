8002a49637367eb28832697616f3c8db4ca602d9
Ivan Miyashiro <ivo.miyashiro1@gmail.com>
2023-07-18 14:18:58 -0300

    Add order service

--- src/order/order.module.ts
+++ src/order/order.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { OrderController } from './controller/order.controller';
+import { OrderService } from './service/order.service';
 
 @Module({
+  providers: [OrderService],
   controllers: [OrderController],
 })
 export class OrdersModule {}
