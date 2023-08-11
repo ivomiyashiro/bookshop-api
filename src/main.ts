e99076dc1e3a6a8d45367efb5a65bda7cf1eab5a
Ivan Miyashiro <ivo.miyashiro1@gmail.com>
2023-08-11 13:41:07 -0300

    Add credentials to cors

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors({
+    credentials: true,
     origin: ['http://localhost:3000'],
   });
   app.use(helmet());
