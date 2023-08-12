cd873849e2bf14c4d962f92a416a902ea0a185b6
Ivan Miyashiro <ivo.miyashiro1@gmail.com>
2023-08-11 21:07:32 -0300

    Fix file names

--- src/common/guard/refresh-token.guard.ts
+++ src/common/guard/refresh-token.guard.ts
@@ -1,24 +1,7 @@
-import { ExecutionContext, Injectable } from '@nestjs/common';
-import { Observable } from 'rxjs';
-import { Reflector } from '@nestjs/core';
 import { AuthGuard } from '@nestjs/passport';
 
-@Injectable()
-export class AtGuard extends AuthGuard('jwt') {
-  constructor(private reflector: Reflector) {
+export class RtGuard extends AuthGuard('jwt-refresh') {
+  constructor() {
     super();
-  }
-
-  canActivate(
-    context: ExecutionContext,
-  ): boolean | Promise<boolean> | Observable<boolean> {
-    const isPublic = this.reflector.getAllAndOverride('isPublic', [
-      context.getHandler(),
-      context.getClass(),
-    ]);
-
-    if (isPublic) return true;
-
-    return super.canActivate(context);
   }
 }
