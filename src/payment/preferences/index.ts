77cf3823781d4e47f23c34c69691ae6d7f74715f
Ivan Miyashiro <ivo.miyashiro1@gmail.com>
2023-08-16 12:54:34 -0300

    Change failure and pending back_urls

--- src/payment/preferences/index.ts
+++ src/payment/preferences/index.ts
@@ -1,8 +1,8 @@
 export const preferences = {
   back_urls: {
     success: 'http://localhost:3000/success',
-    failure: 'http://localhost:3000/failure',
-    pending: 'http://localhost:3000/pending',
+    failure: 'http://localhost:3000/',
+    pending: 'http://localhost:3000/',
   },
   notification_url:
     'https://dev-bookstore.onrender.com/api/payments/notifications',
