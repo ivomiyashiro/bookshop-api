05794ae2f4d48ce1a29a140400511d2226342a3e
Ivan Miyashiro <ivo.miyashiro1@gmail.com>
2023-08-27 16:09:03 -0300

    Add trust proxy

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,19 @@
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 import helmet from 'helmet';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors({
     credentials: true,
     origin: ['http://localhost:3000', 'https://nobugsbooks.vercel.app'],
   });
+  app.set('trust proxy', 1);
   app.use(helmet());
   await app.listen(3030);
 }
+
 bootstrap();
