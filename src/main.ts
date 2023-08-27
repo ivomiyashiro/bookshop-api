aefee180ac334d4ba265c4c416e5f74233036922
Ivan Miyashiro <ivo.miyashiro1@gmail.com>
2023-08-27 16:25:10 -0300

    Add backend current production origin

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,13 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors({
     credentials: true,
-    origin: ['http://localhost:3000', 'https://nobugsbooks.vercel.app'],
+    origin: [
+      'http://localhost:3000',
+      'https://nobugsbooks.vercel.app',
+      'https://dev-bookstore.onrender.com',
+    ],
+    allowedHeaders: 'Content-Type',
+    methods: ['POST', 'GET', 'UPDATE', 'DELETE'],
   });
   app.set('trust proxy', 1);
   app.use(helmet());
